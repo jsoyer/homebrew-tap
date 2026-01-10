fe1fe33a6787d97dc5f2a306a3f2c7c1cc6627ec
Jerome Soyer <jeromesoyer@gmail.com>
2026-01-10 20:37:58 +0100

    Add missing pyarrow dependency and cmake build requirement
    - Add pyarrow resource required by textual-fastdatatable
    - Add cmake as build dependency for pyarrow compilation

--- Formula/sqlit.rb
+++ Formula/sqlit.rb
@@ -7,6 +7,7 @@ class Sqlit < Formula
   sha256 "df90abb5a648a27d9982c9e2ddc903ab6948c831f2ea3fea58df712c175b5197"
   license "MIT"
 
+  depends_on "cmake" => :build
   depends_on "python@3.12"
   depends_on "rust" => :build
 
@@ -23,6 +24,11 @@ class Sqlit < Formula
   resource "pyperclip" do
     url "https://files.pythonhosted.org/packages/30/23/2f0a3efc4d6a32f3b63cdff36cd398d9701d26cda58e3ab97ac79fb5e60d/pyperclip-1.9.0.tar.gz"
     sha256 "b7de0142ddc81bfc5c7507eea19da920b92252b548b96186caf94a5e2527d310"
+  end
+
+  resource "pyarrow" do
+    url "https://files.pythonhosted.org/packages/30/53/04a7fdc63e6056116c9ddc8b43bc28c12cdd181b85cbeadb79278475f3ae/pyarrow-22.0.0.tar.gz"
+    sha256 "3d600dc583260d845c7d8a6db540339dd883081925da2bd1c5cb808f720b3cd9"
   end
 
   resource "keyring" do
